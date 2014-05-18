b87271bd581f0975b60cec606007a41cfc8fe1c3
thaweechok wimonthammawat <thaweechok@giinos.com>
2014-05-18 13:11:52 +0700

    add execute for .java

--- src/com/giinos/phonegap/webview/SSLIgnoredWebView.java
+++ src/com/giinos/phonegap/webview/SSLIgnoredWebView.java
@@ -1,12 +1,25 @@
 package com.giinos.phonegap.webview;
 
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaChromeClient;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class SSLIgnoredWebView extends CordovaPlugin {
 
+	
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+		// TODO Auto-generated method stub
+		
+		this.webView.setWebViewClient(new CustomCordovaWebViewClient(cordova));
+		callbackContext.success("set webview success. " + this.webView.getId());
+		return true;
+	}
 	
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
--- www/ssl-ignored-webview-plugin.js
+++ www/ssl-ignored-webview-plugin.js
@@ -1,5 +1,5 @@
 //var exec = require('cordova/exec');
 
 window.SSLIgnoredWebView = function(arg0, success, error) {
-    cordova.exec(success, error, "SSLIgnoredWebView", "SSLIgnoredWebView", [arg0]);
+    cordova.exec(success, error, "SSLIgnoredWebView", "init", [arg0]);
 };
